9a4e1b222c78bf2fd025e3fb8baf9ef8249cd184
furkan-erhan <osasmortal@gmail.com>
2026-03-01 11:12:52 +0300

    chore: cleaner code structure finalized changes. Closes #2

--- src/BrainFIT.Domain/Entities/Option.cs
+++ src/BrainFIT.Domain/Entities/Option.cs
@@ -7,6 +7,7 @@ public class Option : BaseEntity
     public string Text { get; set; } = string.Empty;
     public bool IsCorrect { get; set; } = false;
     
+    
     public Guid QuestionId { get; set; }
     public Question Question { get; set; } = null!;
 }
